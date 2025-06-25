06425404e9dbdb1e54b5a751e36598eb0ee4295e
heyuncle <heyuncle543@gmail.com>
2025-06-25 15:12:49 -0400

    Setup django project

---
+++ stridesync/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for stridesync project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stridesync.settings')
+
+application = get_wsgi_application()
